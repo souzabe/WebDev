0425e1466c5e055ca39f768e4183955e1015a244
souzabe <jaja013souza@gmail.com>
2024-04-03 22:45:18 -0300

    aula01

---
+++ script.js
@@ -0,0 +1,12 @@
+// alert("ola, Mundo");
+// prompt("digite seu nome");
+// confirm("Você deseja sair");
+
+// console.log("Mauricio")
+
+var nome = prompt("Digite seu nome")
+var idade = prompt("Digite sua idade")
+var profissao = prompt("Digite sua profissão")
+
+console.log(`Olá ${nome}, você tem ${idade} anos e sua profissão é ${profissao}`)
+alert(`Olá ${nome}, você tem ${idade} anos e sua profissão é ${profissao}`)
