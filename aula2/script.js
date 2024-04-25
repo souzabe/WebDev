4bd8e8ac8e9964cc563b56b04e54cbd934a9fc2b
souzabe <jaja013souza@gmail.com>
2024-04-24 22:33:53 -0300

    aula2

---
+++ aula2/script.js
@@ -0,0 +1,14 @@
+// function calculadora(){
+//     var n = document.getElementById('n1').value
+
+//     for(i=0;i<=10;i++){
+//         console.log(n*i)
+//     }
+
+// }
+
+function calculadora(){
+    for(i=100;i>=0;i--){
+    console.log(i)
+    }
+}
